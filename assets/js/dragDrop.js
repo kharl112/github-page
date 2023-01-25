38c8c54507fc4b2cb26c5da7db5185fa05cf4bb5
kharl112 <kharl.dms112@gmail.com>
2023-01-25 07:37:59 -0600

    dragging of alert error fixed

--- assets/js/dragDrop.js
+++ assets/js/dragDrop.js
@@ -13,7 +13,35 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("id");
     const dm = document.getElementById(data);
+    if (!dm) return;
 
     dm.style.left = ev.clientX - 100 + "px";
     dm.style.top = ev.clientY + "px";
 }
+
+
+// for alert 
+var offset = [0, 0];
+var divOverlay = alert_error.firstChild.nextSibling;
+var isDown = false;
+
+divOverlay.addEventListener('mousedown', function (e) {
+    isDown = true;
+    offset = [
+        divOverlay.offsetLeft - e.clientX,
+        divOverlay.offsetTop - e.clientY
+    ];
+}, true);
+
+document.addEventListener('mouseup', function () {
+    isDown = false;
+    console.log("up")
+}, true);
+
+divOverlay.addEventListener('mousemove', function (e) {
+    event.preventDefault();
+    if (isDown) {
+        divOverlay.style.left = (e.clientX + offset[0]) + 'px';
+        divOverlay.style.top = (e.clientY + offset[1]) + 'px';
+    }
+}, true);
